185f4069b88159e52ccdec1fdd9d62c16b95f5c9
Barr <Lakeisha.Barr@bausch.com>
2020-08-03 02:36:21 -0500

    working on updating shuttle requirements

--- script.js
+++ script.js
@@ -9,19 +9,50 @@ window.addEventListener("load", function() {
       let copilotName = document.querySelector("input[name=copilotName]");
       let copilot = copilotName.value;
       let fuelLevel = document.querySelector("input[name=fuelLevel]");
-      let fuel = fuelLevel.value;
+      let fuel = Number(fuelLevel.value);
       let cargoMass = document.querySelector("input[name=cargoMass]");
-      let cargo = cargoMass.value;
+      let cargo = Number(cargoMass.value);
 
       if (pilot === "" || copilot === "" || fuel === "" || cargo === "") {
          alert("All fields are required!");
-         //console.log ("if");  
-         //console.log(pilot);
-      }
-   });
-});
-    
+         console.log ("if");  
+         
+      } else if (isNaN(fuel) || isNaN(cargo)) {
+      alert("number is needed");
+      console.log("else if");
+      } else {
+         //EVERTYTHING ELSE GOES HERE
+        let pilot = document.getElementById(pilotStatus);
+        pilotStatus.innerHTML = `Pilot ${pilotName.value} Ready.`;
 
+        let copilot = document.getElementById(copilotStatus);
+        copilotStatus.innerHTML = `Co-pilot ${copilotName.value} Ready.`;
+
+        let fuel = document.getElementById(fuelStatus < 10000);
+        fuelStatus.innerHTML = `The fuel level is at ${fuelLevel.value}. There is not enough fuel for the journey`;
+        faultyItems.style.visibility = 'visible';
+        document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+        launchStatusCheck.style.color = 'red';
+        console.log(fuelStatus);
+        
+        let cargo = document.getElementById(cargoMass > 10000);
+         cargoStatus.innerHTML = `The cargo mass is at ${cargoMass.value}. There is too much cargo for the journey`; 
+         faultyItems.style.visibility = 'visible'; 
+         document.getElementById("launchStatus").innerHTML = "Shuttle not ready for launch.";
+         launchStatusCheck.style.color = 'red';
+         console.log(cargoMass);
+          /*if (fuelLevel < 10000) {
+            faultyItems.style.visibility = 'visible';
+            launchStatus = "Shuttle not ready for launch."
+            launchStatusCheck.style.color = 'red';
+            console.log(`The fuel level is at ${fuelStatus}. There is not enough fuel for the journey`);
+         */
+          }
+      })
+   });
+;
+
+  
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
